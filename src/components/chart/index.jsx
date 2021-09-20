809b4ccc7031d22b1b727ff422519aab77474fcc
VuTan115 <tanboy042001@gmail.com>
2021-09-21 01:29:04 +0700

    get prefectures data and generate checkbox

--- src/components/chart/index.jsx
+++ src/components/chart/index.jsx
@@ -1,23 +1,68 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Chart from 'react-apexcharts'
+import instance from '../../../api/axios'
+import endPoints from '../../enums/endpoints'
+
 // 人口数 都道府県
 export default function MyChart() {
   const [prefectures, setPrefectures] = useState([])
 
   /* chart config */
   const options = {
-    chart: {},
+    chart: {
+      // redrawOnParentResize: true,
+      toolbar: {
+        show: true,
+        offsetX: 0,
+        offsetY: 0,
+        tools: {
+          download: true,
+          selection: true,
+          zoom: true,
+          zoomin: true,
+          zoomout: true,
+          pan: true,
+          reset: true,
+          customIcons: []
+        },
+        autoSelected: 'none'
+      }
+    },
     stroke: {
       lineCap: 'butt'
     },
     xaxis: {
       type: 'category',
       show: true,
-      categories: []
+      categories: [],
+      labels: {
+        show: true,
+        offsetX: 0,
+        style: {
+          cssClass: 'xaxis-label'
+        }
+      },
+      title: {
+        text: '年度',
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+          cssClass: 'xaxis-title axis-title'
+        }
+      }
     },
     yaxis: {
+      type: 'category',
+      show: true,
       showAlways: true,
-      categories: []
+      categories: [],
+      title: {
+        rotate: 360,
+        text: '人口数',
+        style: {
+          cssClass: 'yaxis-title axis-title'
+        }
+      }
     },
     legend: {
       offsetY: 80,
@@ -35,11 +80,38 @@ export default function MyChart() {
     const prefName = e.target.value
     const checkStatus = e.target.checked
   }
+  useEffect(async () => {
+    try {
+      const response = await instance.get(endPoints.PREFECTURES)
+      setPrefectures(response.data.result)
+      return ''
+    } catch (error) {
+      return error
+    }
+  }, [])
 
   return (
     <>
       <div className="container ">
         <div className="card">
+          <div className="filter" onChange={checkboxChecked}>
+            {prefectures.map((prefecture) => (
+              <label
+                className="filter-items"
+                htmlFor={prefecture.prefCode}
+                key={prefecture.prefCode}
+              >
+                <input
+                  type="checkbox"
+                  name="filter-items"
+                  id={prefecture.prefCode}
+                  value={prefecture.prefName}
+                  defaultChecked={prefecture.prefCode === 1}
+                />
+                <span className="label">{prefecture.prefName}</span>
+              </label>
+            ))}
+          </div>
           <div id="chart">
             <Chart
               options={{ ...options }}
